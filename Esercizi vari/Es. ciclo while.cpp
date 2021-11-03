9ead5af05141cdec860d92237539254a668674db
Pall1n <75300377+Pall1n@users.noreply.github.com>
2021-11-03 12:09:56 +0100

    Add files via upload

---
+++ Esercizi vari/Es. ciclo while.cpp
@@ -0,0 +1,16 @@
+//Consegna: Implementare un programma che stampi i numeri da 1 a 200 usando un ciclo while.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 1;
+    while(i <= 200){
+        cout<<i<<endl;
+        i++;
+    }
+    cout<<"Fine!";
+    return 0;
+}
