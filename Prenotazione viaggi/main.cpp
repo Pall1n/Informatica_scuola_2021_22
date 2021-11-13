aa685a3e6c3a0278d98c4c36bf3fa6a968fc01f4
Pall1n <75300377+Pall1n@users.noreply.github.com>
2021-11-13 18:16:58 +0100

    Update main.cpp

--- Prenotazione viaggi/main.cpp
+++ Prenotazione viaggi/main.cpp
@@ -3,23 +3,17 @@ Un'agenzia di viaggi effettua le prenotazioni per i voli della compagnia Rapisar
 1) Catania-Roma 30€;
 2) Catania-Praga 55€;
 3) Catania-New York 400€.
-
 Al momento della prenotazione il cliente dovrà indicare il numero di passeggeri da prenotare e l'eventuale supplemento del bagaglio in stiva, che corrisponde a €20 per 
 le destinazioni europee e €50 per i voli intercontinentali.
-
 Nel mese di Novembre l'agenzia applica uno sconto del 10% se il costo della prenotazione è superiore a €500.
-
 Sul costo della prenotazione bisognerà calcolare le imposte di imbarco del 10% per destinazioni europee e del 17% per destinazioni intercontinentali.
-
 Visualizzare:
 1) Il totale lordo della prenotazione (con i bagagli);
 2) Le imposte di imbarco;
 3) Lo sconto;
 4) Il totale netto da pagare;
 5) Il messaggio "Sconto d'autunno" se è stato applicato lo sconto di Novembre.
-
 Alla fine il programma dovrà prevedere il controllo dell'input in modo tale che non possano essere inseriti codici di destinazione diversi da quelli stabiliti.
-
 Se il cliente ha bagagli in stiva, visualizzare il messaggio: "Recarsi per il check-in allo sportello 8".
 */
 
@@ -32,11 +26,25 @@ Se il cliente ha bagagli in stiva, visualizzare il messaggio: "Recarsi per il ch
 
 using namespace std;
 
-string partenze[] = {"Catania Fontanarossa"};
-int lenght_partenze = sizeof(partenze)/sizeof(partenze[0]);
-string destinazioni_CT[] = {{"Roma Fiumicino", "Europeo", "30€"}, {"Praga Václav Havel", "Europeo", "55€"}, {"New York John F. Kennedy International", "Intercontinentale", "400€"}};
-int lenght_destinazioni_CT = sizeof(destinazioni_CT)/sizeof(destinazioni_CT[0]);
-int scelta;
+string partenze[] = {
+    "Catania Fontanarossa",
+    "ciao"
+};
+
+#define length_partenze sizeof(partenze)/sizeof(partenze[0])
+
+string destinazioni[][10][3] = { //Massimo destinazioni per ogni partenza: 10
+    {
+        {"Roma Fiumicino", "Europeo", "30"},
+        {"Praga Vàclav Havel", "Europeo", "55"},
+        {"New York John F. Kennedy International", "Intercontinentale", "400"}
+    },
+    {
+        {"Francia", "Europeo", "80"},
+        {"All'inferno", "Intercontinentale", "0"},
+        {"test", "testing", "tested"}
+    }
+};
 
 class Programma{
     public:
@@ -50,66 +58,55 @@ class Programma{
     }
     
     void partenze_scelta(){
-        scelta = 0;
-        while(scelta <= 0 || scelta > lenght_partenze){
+        int scelta_partenza = 0;
+        while(scelta_partenza <= 0 && scelta_partenza <= length_partenze){
             cout<<"================================\n"
                   "|           PARTENZE           |\n"
                   "================================\n";
                   
             cout<<"\nBenvenuto, da dove vuoi partire?\n";
-            for(int i = 0; i < lenght_partenze; i++){
+            for(int i = 0; i < length_partenze; i++){
                 cout<<i+1<<") "<<partenze[i]<<endl;
             }
             cout<<"\nIndica il numero della scelta: ";
-            cin>>scelta;
+            cin>>scelta_partenza;
             
-            if(scelta > 0 && scelta <= lenght_partenze){
-                clear();
-                switch_partenze(scelta);
-            }
-            else{
+            if(scelta_partenza <= 0 && scelta_partenza <= length_partenze){
                 cout<<"\nScelta non valida, riprova.\n";
                 sleep(2);
                 clear();
             }
         }
+        clear();
+        destinazioni_funz(scelta_partenza-1);
     }
     
-    void switch_partenze(char scelta){
-        switch(scelta){
-            case 1:
-            destinazioni_catania();
-            break;
-        }
-    }
-    
-    void destinazioni_catania(){
-        scelta = 0;
-        while(scelta <= 0 || scelta > lenght_destinazioni_CT){
-            cout<<"================================\n"
-                  "|         DESTINAZIONI         |\n"
-                  "================================\n";
+    void destinazioni_funz(int scelta_partenza){
+        int scelta_destinazione = 0;
+        int ordine = 1;
+        while(scelta_destinazione <= 0 && scelta_destinazione <= length_partenze){ //sistemare giusto
+            cout<<"====================================\n"
+                  "|           DESTINAZIONI           |\n"
+                  "====================================\n";
                   
             cout<<"\nQual'è la destinazione desiderata?\n";
-            for(int i = 0; i < lenght_destinazioni_CT; i++){
-                cout<<i+1<<") "<<destinazioni_catania[0][0]<<endl;
+            for(auto i = begin(destinazioni[scelta_partenza]); i < end(destinazioni[scelta_partenza]); i++){
+                if(*i[0] != "") cout<<ordine<<") "<<*i[0]<<endl;
+                ordine++;
             }
             cout<<"\nIndica il numero della scelta: ";
-            cin>>scelta;
+            cin>>scelta_destinazione;
             
-            if(scelta > 0 && scelta <= lenght_destinazioni_CT){
-                clear();
-                switch_destinazioni_CT(scelta);
-            }
-            else{
+            if(scelta_destinazione <= 0 && scelta_destinazione <= length_partenze){
                 cout<<"\nScelta non valida, riprova.\n";
                 sleep(2);
                 clear();
             }
         }
+        clear();
+        //passeggeri_funz(destinazioni_CT); !!!!!!!!!!
     }
 };
-
 
 int main()
 {
