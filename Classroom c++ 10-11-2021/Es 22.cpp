4f063dd7ab42bfbc8372756851633abd825f6014
Pall1n <75300377+Pall1n@users.noreply.github.com>
2021-11-26 10:47:36 +0100

    Add files via upload

---
+++ Classroom c++ 10-11-2021/Es 22.cpp
@@ -0,0 +1,31 @@
+/*Consegna: 
+Determinare il fattoriale di un numero intero N;
+valgono le seguenti relazioni:
+N! = N(N-1)(N-2)... 1, 0! = 1, 1! = 1
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double N;
+    double somma = 1;
+    
+    cout<<"Inserisci un numero per trovare il suo fattoriale: ";
+    cin>>N;
+    
+    if(N == 0) cout<<"Il fattoriale del numero inserito equivale a 1";
+    else if(N == 1) cout<<"Il fattoriale del numero inserito equivale a 1";
+    else if(N < 0) cout<<"Il programma accetta solo numeri maggiori o uguali a 0";
+    else{
+        int i = N;
+        while(i > 0){
+            i--;
+            somma *= N-i;
+        }
+        cout<<"Il fattoriale del numero inserito equivale a "<<somma;
+    }
+    
+    return 0;
+}
