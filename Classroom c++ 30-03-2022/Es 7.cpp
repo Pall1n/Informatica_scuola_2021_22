ab59361d1c583c94fcb25e16ef599a2c24950ed8
Pall1n <75300377+Pall1n@users.noreply.github.com>
2022-04-08 10:44:54 +0200

    Add files via upload

---
+++ Classroom c++ 30-03-2022/Es 7.cpp
@@ -0,0 +1,23 @@
+/*Consegna:
+Creare una funzione che restituisce se un numero intero è naturale o meno
+*/
+
+#include <iostream>
+
+using namespace std;
+
+bool verifica(int n){
+    return (n >= 0) ? true : false;
+}
+
+int main(){
+    int n;
+    
+    cout<<"Inserisci un numero: ";
+    cin>>n;
+
+    if(verifica(n)) cout<<"Il numero inserito appartiene all'insieme dei numeri naturali";
+    else cout<<"Il numero inserito non appartiene all'insieme dei numeri naturali";
+
+    return 0;
+}
