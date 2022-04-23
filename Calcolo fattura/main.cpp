260d74aa4130253c95f890feec41e333a8f2b115
Pall1n <75300377+Pall1n@users.noreply.github.com>
2022-04-23 12:43:34 +0200

    Update main.cpp

--- Calcolo fattura/main.cpp
+++ Calcolo fattura/main.cpp
@@ -43,14 +43,14 @@ void clear() {
 
 const string colore_reset = "\033[0m";
 
-unsigned int quantita;
-unsigned short int aliquota_iva;
+int quantita;
+short int aliquota_iva;
 double prezzo_unitario, iva_costo, imponibile, sconto_interesse, totale_lordo, totale_netto;
 string modalita_pagamento;
 bool uscire = false;
 
-int verifica_inserimento_quantita(unsigned int& x) {
-	while (cin.fail()) {
+int verifica_inserimento_quantita(int& x) {
+	while (cin.fail() || x <= 0) {
 		cin.clear();
 		cout << "Inserisci una quantità valida! ";
 		cin.ignore(10000, '\n');
@@ -69,8 +69,8 @@ double verifica_inserimento_prezzo(double& x) {
 	return x;
 }
 
-int verifica_inserimento_aliquota(unsigned short int& x) {
-	while (cin.fail() || x > 100) {
+int verifica_inserimento_aliquota(short int& x) {
+	while (cin.fail() || x > 100 || x < 1) {
 		cin.clear();
 		cout << "Inserisci un'aliquota valida! ";
 		cin.ignore(10000, '\n');
@@ -107,22 +107,36 @@ double calcolo_totale_netto() {
 	return totale_lordo + sconto_interesse;
 }
 
-void risultati() {
-	clear();
-	cout << "\033[1;96m" << "===============================================\n"
-		"|                   FATTURA                   |\n"
-		"===============================================\n\n" << colore_reset <<
-		"Imponibile: " << imponibile << endl <<
-		"IVA: " << iva_costo << endl <<
-		"Totale lordo: " << totale_lordo << endl;
-	if (sconto_interesse > 0) {
-		cout << "Interessi: " << sconto_interesse << endl;
+void homepage() {
+	modalita_pagamento = "";
+	while (modalita_pagamento != "contanti" && modalita_pagamento != "30gg" && modalita_pagamento != "30gg+") {
+		cout << "\033[1;96m" << "=====================================================\n"
+			"|                  CALCOLO FATTURA                  |\n"
+			"=====================================================\n\n" << colore_reset <<
+			"Inserisci la quantità: ";
+		cin >> quantita;
+		verifica_inserimento_quantita(quantita);
+		cout << "Inserisci il prezzo unitario: ";
+		cin >> prezzo_unitario;
+		verifica_inserimento_prezzo(prezzo_unitario);
+		cout << "Inserisci l'aliquota IVA: ";
+		cin >> aliquota_iva;
+		verifica_inserimento_aliquota(aliquota_iva);
+		cout << "Inserisci la modalità di pagamento: ";
+		cin >> modalita_pagamento;
+
+		if (modalita_pagamento != "contanti" && modalita_pagamento != "30gg" && modalita_pagamento != "30gg+") {
+			cout << "\nModalità di pagamento non valida, riprova!\n";
+			sleep(2);
+			clear();
+		}
 	}
-	else {
-		sconto_interesse *= -1;
-		cout << "Sconto: " << sconto_interesse << endl;
-	}
-	cout << "Totale netto: " << totale_netto << endl;
+	
+	imponibile = calcolo_imponibile();
+	iva_costo = calcolo_iva();
+	totale_lordo = calcolo_totale_lordo();
+	sconto_interesse = calcolo_sconto();
+	totale_netto = calcolo_totale_netto();
 }
 
 void uscita() {
@@ -149,45 +163,30 @@ void uscita() {
 	} while (scelta_uscita != "no" && scelta_uscita != "si");
 }
 
-void homepage() {
-	while(!uscire){
-		modalita_pagamento = "";
-		while (modalita_pagamento != "contanti" || modalita_pagamento != "30gg" || modalita_pagamento != "30gg+") {
-			cout << "\033[1;96m" << "=====================================================\n"
-				"|                  CALCOLO FATTURA                  |\n"
-				"=====================================================\n\n" << colore_reset <<
-				"Inserisci la quantità: ";
-			cin >> quantita;
-			verifica_inserimento_quantita(quantita);
-			cout << "Inserisci il prezzo unitario: ";
-			cin >> prezzo_unitario;
-			verifica_inserimento_prezzo(prezzo_unitario);
-			cout << "Inserisci l'aliquota IVA: ";
-			cin >> aliquota_iva;
-			verifica_inserimento_aliquota(aliquota_iva);
-			cout << "Inserisci la modalità di pagamento: ";
-			cin >> modalita_pagamento;
-
-			if (modalita_pagamento != "contanti" || modalita_pagamento != "30gg" || modalita_pagamento != "30gg+") {
-				cout << "\nModalità di pagamento non valida, riprova!\n";
-				sleep(2);
-				clear();
-			}
-		}
-		
-		imponibile = calcolo_imponibile();
-		iva_costo = calcolo_iva();
-		totale_lordo = calcolo_totale_lordo();
-		sconto_interesse = calcolo_sconto();
-		totale_netto = calcolo_totale_netto();
-		
-		risultati();
-		uscita();
+void risultati() {
+	clear();
+	cout << "\033[1;96m" << "===============================================\n"
+		"|                   FATTURA                   |\n"
+		"===============================================\n\n" << colore_reset <<
+		"Imponibile: " << imponibile << endl <<
+		"IVA: " << iva_costo << endl <<
+		"Totale lordo: " << totale_lordo << endl;
+	if (sconto_interesse > 0) {
+		cout << "Interessi: " << sconto_interesse << endl;
 	}
+	else {
+		sconto_interesse *= -1;
+		cout << "Sconto: " << sconto_interesse << endl;
+	}
+	cout << "Totale netto: " << totale_netto << endl;
 }
 
 int main()
 {
-	homepage();
+    while(!uscire){
+	    homepage();
+	    risultati();
+	    uscita();
+    }
 	return 0;
 }
