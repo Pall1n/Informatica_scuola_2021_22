282e1e4502626acdc0c620a827bbefb6947c3798
Pall1n <75300377+Pall1n@users.noreply.github.com>
2022-05-12 18:24:08 +0200

    Update main.cpp

--- Busta paga/main.cpp
+++ Busta paga/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-float calcolo_lordo(float ore, float paga_oraria){
+float calcolo_lordo(float ore, float paga_oraria) {
 	return ore * paga_oraria;
 }
 
@@ -13,27 +13,31 @@ float calcolo_imponibile(float stipendio_lordo, float ritenute) {
 	return stipendio_lordo - ritenute;
 }
 
-float calcolo_imposta_lorda(float imponibile) {
+float calcolo_IRPEF(float imponibile) {
 	if (imponibile <= 1154) {
 		return imponibile * 23 / 100;
-	} else if (imponibile > 1154 && imponibile <= 2154) {
+	}
+	else if (imponibile > 1154 && imponibile <= 2154) {
 		return imponibile * 25 / 100;
-	} else {
+	}
+	else {
 		return imponibile * 35 / 100;
 	}
 }
 
-float calcolo_detrazioni_dipendente(float imponibile, float reddito) {
+float calcolo_detrazioni_dipendente(float imponibile) {
 	if (imponibile <= 1154) {
 		return 144;
-	} else if (imponibile > 1154 && imponibile <= 2154) {
-		return 146 + 92 * (2154 - reddito) / 1000;
-	} else {
+	}
+	else if (imponibile > 1154 && imponibile <= 2154) {
+		return 146 + 92 * (2154 - imponibile) / 1000;
+	}
+	else {
 		return 0;
 	}
 }
 
-float calcolo_detrazioni_familiari(int *figli) {
+float calcolo_detrazioni_familiari(int* figli) {
 	int tot = 0;
 	for (int i = 0; i < sizeof(figli) / sizeof(figli[0]); i++) {
 		switch (i) {
@@ -52,19 +56,25 @@ float calcolo_detrazioni_familiari(int *figli) {
 float calcolo_detrazione_coniuge(float imponibile) {
 	if (imponibile <= 1154) {
 		return 67;
-	} else if (imponibile > 1154 && imponibile <= 3333) {
+	}
+	else if (imponibile > 1154 && imponibile <= 3333) {
 		return 58;
-	} else {
+	}
+	else {
 		return 0;
 	}
 }
 
-float calcolo_imposta_netta() {
-	
+float somma_detrazioni(float aliquota, float irpef, float detrazioni_dipendente, float detrazioni_familiari, float detrazioni_coniuge) {
+	return aliquota + irpef + detrazioni_dipendente + detrazioni_familiari + detrazioni_coniuge;
 }
 
-int main(){
-	
-	
+float calcolo_stipendio_netto(float imponibile, float totale_detrazioni) {
+	return imponibile - totale_detrazioni;
+}
+
+int main() {
+
+
 	return 0;
 }
